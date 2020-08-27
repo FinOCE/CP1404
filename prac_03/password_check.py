e2155a498fd6eb5e481329af6fcf2d5e48b3787e
Finley Sherwood <me@itsf.in>
2020-08-27 21:04:30 +1000

    Refactor password_check.py to use functions

--- prac_03/password_check.py
+++ prac_03/password_check.py
@@ -1,9 +1,19 @@
 MIN_LENGTH = 3
 
-password = input("Enter password: ")
-while len(password) < MIN_LENGTH:
-    print(f"Password must be at least {MIN_LENGTH} characters long")
-    password = input("Enter password: ")
+def main():
+    password = get_password()
+    print(convert_to_asterisk(password))
 
-for i in range(0, len(password)):
-    print("*", end="")
+def get_password():
+    password = input("Enter password: ")
+    while len(password) < MIN_LENGTH:
+        print(f"Password must be at least {MIN_LENGTH} characters long")
+        password = input("Enter password: ")
+    
+    return password
+
+def convert_to_asterisk(password):
+    new_password = len(password) * "*"
+    return new_password
+
+main()
